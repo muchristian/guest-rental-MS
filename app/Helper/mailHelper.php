0dafd024c2b3d81181da5f3950c7f04c100561f5
muchristian <mucyochristian2@gmail.com>
2020-06-17 01:56:49 +0200

    changes

--- app/Helper/mailHelper.php
+++ app/Helper/mailHelper.php
@@ -7,9 +7,9 @@ use Mail;
 trait mailHelper {
     public function sendMail($view, $title, $subject, $attr, $email, $code, $url) {
         return Mail::send($view, ['firstName'=> $attr, "{$code}"=>$url], function($mail) use ($email) {
-            $mail->from('markjoker73@gmail.com', "{$title}");
+            $mail->from('markjoker73@gmail.com', $title);
             $mail->to($email);
-            $mail->subject("{$subject}");
+            $mail->subject($subject);
           });
     }
 }
