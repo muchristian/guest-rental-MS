38ae579e002eaac5df52b349ff0ef79c945c4691
muchristian <mucyochristian2@gmail.com>
2020-06-18 03:16:52 +0200

    change

--- app/Helper/RoleHelper.php
+++ app/Helper/RoleHelper.php
@@ -4,6 +4,6 @@ namespace App\Helper;
 
 class RoleHelper {
     public static function Roles() {
-        return array('MANAGER', 'ADMIN/OWNER', 'SUPER_ADMIN');
+        return array('MANAGER', 'ADMIN', 'SUPER_ADMIN');
     }
 }
