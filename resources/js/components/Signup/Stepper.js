b3dc60aa07777c37ef0f8ba73bb3567961b67cdc
muchris <mucyochristian2@gmail.com>
2023-05-10 12:30:08 +0200

    clean up codes v1

--- resources/js/components/Signup/Stepper.js
+++ resources/js/components/Signup/Stepper.js
@@ -1,60 +1,55 @@
-import React, {Component, Fragment} from 'react';
-import Form from '../UI/Form';
-import Button from '../UI/Button';
-import { Link } from 'react-router-dom';
-import ProgressStepper from '../UI/ProgressStepper';
+import React, { Component, Fragment } from "react";
+import Form from "../UI/Form";
+import Button from "../UI/Button";
+import { Link } from "react-router-dom";
+import ProgressStepper from "../UI/ProgressStepper";
 
 export default class Stepper extends Component {
     state = {
-        steps: []
-    }
+        steps: [],
+    };
     indexOfHandler(val) {
-      return Object.keys(this.props.stepper).indexOf(val)
+        return Object.keys(this.props.stepper).indexOf(val);
     }
-      assignStepper() {
+    assignStepper() {
         let stepperArr = [];
-        for(let key in this.props.stepper) {
-            console.log(key);
+        for (let key in this.props.stepper) {
             stepperArr.push({
                 id: key,
                 active: key === this.props.currentStep ? true : false,
-                approval: this.indexOfHandler(key) < this.indexOfHandler(this.props.currentStep) && 'approved'
-            })
+                approval:
+                    this.indexOfHandler(key) <
+                        this.indexOfHandler(this.props.currentStep) &&
+                    "approved",
+            });
         }
-        this.setState({steps: stepperArr})
-      }
-      componentDidMount() {
+        this.setState({ steps: stepperArr });
+    }
+    componentDidMount() {
         /*
           let obj = {
             ...this.props.signup[this.props.currentStep]
           }
           this.setState({sign: obj})
           */
-          this.assignStepper()
-      }
-      componentDidUpdate(prevProps) {
+        this.assignStepper();
+    }
+    componentDidUpdate(prevProps) {
         if (prevProps.currentStep !== this.props.currentStep) {
-            this.assignStepper()
+            this.assignStepper();
         }
-      }
-render() {
-    const {steps} = this.state;
-    console.log(steps)
+    }
+    render() {
+        const { steps } = this.state;
         let formElement = (
             <Fragment>
-                
-                            <ul id="progressStepper">
-                                {steps.map(st => (
-                                    <ProgressStepper key={st.id} step={st}/>
-                                ))}
-                            </ul>
-                            
-                </Fragment>
+                <ul id="progressStepper">
+                    {steps.map((st) => (
+                        <ProgressStepper key={st.id} step={st} />
+                    ))}
+                </ul>
+            </Fragment>
         );
-    return (
-        <Fragment>
-            {formElement}
-        </Fragment>
-    )
-}
+        return <Fragment>{formElement}</Fragment>;
+    }
 }
--- resources/js/components/UI/Panel.js
+++ resources/js/components/UI/Panel.js
@@ -1,48 +1,44 @@
-import React, { Component, Fragment } from 'react';
-import PanelComponent from './PanelComponent';
+import React, { Component, Fragment } from "react";
+import PanelComponent from "./PanelComponent";
 
 export default class Panel extends Component {
-
     componentDidMount() {
-        let el  = document.getElementsByClassName('menu-collapsed');
-        let el1 = document.getElementsByClassName('submenu-icon');
-        let el2 = document.getElementsByClassName('sidebar-submenu');
-        for(let i = 0; i < el.length; i++) {
+        let el = document.getElementsByClassName("menu-collapsed");
+        let el1 = document.getElementsByClassName("submenu-icon");
+        let el2 = document.getElementsByClassName("sidebar-submenu");
+        for (let i = 0; i < el.length; i++) {
             if (this.props.sidebar_toggle) {
-                el[i].classList.add('d-none');
+                el[i].classList.add("d-none");
                 if (el1[i] && el2[i]) {
-                el1[i].classList.add('d-none');
-                el2[i].classList.add('d-none');
+                    el1[i].classList.add("d-none");
+                    el2[i].classList.add("d-none");
                 }
             } else {
-                el[i].classList.remove('d-none');
+                el[i].classList.remove("d-none");
                 if (el1[i] && el2[i]) {
-                    el1[i].classList.remove('d-none');
-                    el2[i].classList.remove('d-none');
+                    el1[i].classList.remove("d-none");
+                    el2[i].classList.remove("d-none");
                 }
-                
             }
-            
         }
     }
     componentDidUpdate(prevProps) {
-        let el  = document.getElementsByClassName('menu-collapsed');
-        let el1 = document.getElementsByClassName('submenu-icon');
-        let el2 = document.getElementsByClassName('sidebar-submenu');
+        let el = document.getElementsByClassName("menu-collapsed");
+        let el1 = document.getElementsByClassName("submenu-icon");
+        let el2 = document.getElementsByClassName("sidebar-submenu");
         if (prevProps.sidebar_toggle !== this.props.sidebar_toggle) {
-            for(let i = 0; i < el.length; i++) {
-                console.log(el1[i]);
+            for (let i = 0; i < el.length; i++) {
                 if (this.props.sidebar_toggle) {
-                    el[i].classList.add('d-none');
+                    el[i].classList.add("d-none");
                     if (el1[i] && el2[i]) {
-                        el1[i].classList.add('d-none');
-                        el2[i].classList.add('d-none');
+                        el1[i].classList.add("d-none");
+                        el2[i].classList.add("d-none");
                     }
                 } else {
-                    el[i].classList.remove('d-none');
+                    el[i].classList.remove("d-none");
                     if (el1[i] && el2[i]) {
-                    el1[i].classList.remove('d-none');
-                    el2[i].classList.remove('d-none');
+                        el1[i].classList.remove("d-none");
+                        el2[i].classList.remove("d-none");
                     }
                 }
             }
@@ -50,35 +46,46 @@ export default class Panel extends Component {
     }
 
     render() {
-        console.log(this.props.sidebar_toggle)
         let element = null;
-        //element = <PanelComponent panel="ADMIN"></PanelComponent>
         if ("ADMIN") {
-            element = <PanelComponent panel="ADMIN"></PanelComponent>
+            element = <PanelComponent panel="ADMIN"></PanelComponent>;
         }
-        return ( 
-<Fragment>
-    <div id="sidebar-container" className={`bg-dark ${this.props.sidebar_toggle ? 'sidebar-collapsed' : 'sidebar-expanded'}`}>
-         
-        <ul className="list-group"> 
-            <li className={`list-group-item sidebar-separator-title text-muted align-items-center menu-collapsed ${this.props.sidebar_toggle && 'd-none'} `}>
-        <small>MAIN MENU</small>
-            </li> 
-            <li>
-            <a href="#" className="list-group-item list-group-item-action bg-dark">
-                <div className="d-flex w-100 justify-content-start align-items-center">
-                    <span className="ti-dashboard mr-3"></span>
-                    <span className="text-xs menu-collapsed">Dashboard</span>
+        return (
+            <Fragment>
+                <div
+                    id="sidebar-container"
+                    className={`bg-dark ${
+                        this.props.sidebar_toggle
+                            ? "sidebar-collapsed"
+                            : "sidebar-expanded"
+                    }`}
+                >
+                    <ul className="list-group">
+                        <li
+                            className={`list-group-item sidebar-separator-title text-muted align-items-center menu-collapsed ${
+                                this.props.sidebar_toggle && "d-none"
+                            } `}
+                        >
+                            <small>MAIN MENU</small>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="list-group-item list-group-item-action bg-dark"
+                            >
+                                <div className="d-flex w-100 justify-content-start align-items-center">
+                                    <span className="ti-dashboard mr-3"></span>
+                                    <span className="text-xs menu-collapsed">
+                                        Dashboard
+                                    </span>
+                                </div>
+                            </a>
+                        </li>
+
+                        {element}
+                    </ul>
                 </div>
-            </a>
-            </li>
-            
-            
-                
-     {element}
-        </ul>
-    </div>
- </Fragment>
-        )
+            </Fragment>
+        );
     }
 }
